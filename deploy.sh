acb1f9a2b9955d3491085bedc55904e3b883dace
jianfeng <gjf610@yeah.net>
2022-04-20 00:16:23 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ set -e
 pnpm build &&
 git add . &&
 git commit -m 'deploy' &&
-git branch -M main &&
-git remote add origin git@github.com:gjf610/CV.git
-git push -f -u origin main
+git push
 cd -
 echo https://gjf610.github.io/CV/dist/index.html
